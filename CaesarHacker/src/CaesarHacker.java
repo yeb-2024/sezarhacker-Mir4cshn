a9eae93a747ccfda5b1c3a069857286d260d655e
Mir4cshn <159525962+Mir4cshn@users.noreply.github.com>
2025-02-28 18:03:36 +0300

    panel1 ve messagefield eklendi

--- CaesarHacker/src/CaesarHacker.java
+++ CaesarHacker/src/CaesarHacker.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 public class CaesarHacker implements ActionListener {
     private String SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private JFrame frame;
+    private JTextField messageField;
 
     CaesarHacker(){
         frame = new JFrame("Caesar Şifreleme Hacker");
@@ -13,9 +14,14 @@ public class CaesarHacker implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(3, 1));
 
+        JPanel panel1 = new JPanel(new FlowLayout());
+        JLabel label1 = new JLabel("Şifreli Mesaj:");
+        label1.setFont(new Font("Arial", Font.PLAIN, 16));
+        messageField = new JTextField(20);
+        panel1.add(label1);
+        panel1.add(messageField);
 
-
-
+        frame.add(panel1);
         frame.setVisible(true);
     }
 
